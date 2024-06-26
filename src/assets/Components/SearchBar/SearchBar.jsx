b731879c79d6bc3ff923eca76e1c27afed770db0
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-06-26 11:17:59 +0800

    tweaked some stuff

--- src/assets/Components/SearchBar/SearchBar.jsx
+++ src/assets/Components/SearchBar/SearchBar.jsx
@@ -5,23 +5,27 @@ import './SearchBar.css'
 
 const SearchBar = ({fetch}) => {
   const [name, setName] = useState("pikachu")
+  var searchInput
+
   function assignValue(word) {
-    var x = word.toLowerCase()
-    setName(x)
+    searchInput = word.toLowerCase()
+    setName(searchInput)
   }
+
   const handleSubmit = (e) => {
     e.preventDefault()
+    assignValue(name)
     fetch(name)
   }
 
   return (
     <form onSubmit={handleSubmit}>
-        <input type="text" id='search' value={name} onChange={(e) => assignValue(e.target.value)}/>
+        <input type="text" id='search' value={name} onChange={(e) => setName(e.target.value)}/>
         <button type="submit" id="searchBtn" placeholder="Search...">
           <img src="https://img.icons8.com/?size=100&id=DZe3wFKTc8IK&format=png&color=FC405C" alt="" />
         </button>
     </form>
-
+ 
   )
 }
 
