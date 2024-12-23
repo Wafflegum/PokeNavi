4f46216e9434743c39703795206833af92ef3677
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-12-23 10:21:05 +0800

    Added mobile layout responsiveness

--- src/assets/Components/SearchBar/SearchBar.jsx
+++ src/assets/Components/SearchBar/SearchBar.jsx
@@ -1,32 +1,30 @@
-import React, { useState } from 'react'
-import ResultCard from '../SearchCard/ResultCard'
+import { useState } from 'react'
 
 import './SearchBar.css'
 
-const SearchBar = ({fetch}) => {
-  const [name, setName] = useState("pikachu")
-  var searchInput
+const SearchBar = ({ fetch }) => {
+	const [name, setName] = useState('pikachu')
+	var searchInput
 
-  function assignValue(word) {
-    searchInput = word.toLowerCase()
-    setName(searchInput)
-  }
+	function assignValue(word) {
+		searchInput = word.toLowerCase()
+		setName(searchInput)
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    assignValue(name)
-    fetch(name)
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		assignValue(name)
+		fetch(name)
+	}
 
-  return (
-    <form onSubmit={handleSubmit}>
-        <input type="text" id='search' value={name} onChange={(e) => setName(e.target.value)}/>
-        <button type="submit" id="searchBtn" placeholder="Search...">
-          <img src="https://img.icons8.com/?size=100&id=DZe3wFKTc8IK&format=png&color=FC405C" alt="" />
-        </button>
-    </form>
- 
-  )
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="text" id="search" value={name} onChange={(e) => setName(e.target.value)} />
+			<button type="submit" id="searchBtn" placeholder="Search...">
+				<img src="https://img.icons8.com/?size=100&id=DZe3wFKTc8IK&format=png&color=FC405C" alt="" />
+			</button>
+		</form>
+	)
 }
 
 export default SearchBar
--- src/assets/Components/SearchCard/ResultCard.jsx
+++ src/assets/Components/SearchCard/ResultCard.jsx
@@ -1,130 +1,210 @@
-import React, { useState } from 'react'
+/* eslint-disable no-mixed-spaces-and-tabs */
 import './ResultCard.css'
 
 const ResultCard = ({ name, image, image2, height, weight, health, attack, defense, sp, sd, speed, type }) => {
-  function toTitleCase(word) {
-    if (word)
-    return word[0].toUpperCase() + word.slice(1).toLowerCase()
-  }
-  const pokemonName = toTitleCase(name)
+	function toTitleCase(word) {
+		if (word) return word[0].toUpperCase() + word.slice(1).toLowerCase()
+	}
+	const pokemonName = toTitleCase(name)
 
-  if(type)
-    var decoratorColorID = type[0].type.name // this will get the first type's id. 
+	if (type) var decoratorColorID = type[0].type.name // this will get the first type's id.
 
-  return (
-    <div className="card-container">
+	return (
+		<div className="card-container">
+			<div className="preview">
+				<div
+					className="decorator"
+					id={decoratorColorID ? decoratorColorID : 'fighting'}
+					style={
+						name
+							? {
+									'--clip-path': 'polygon(100% 0, 0 0, 0 100%, 0 100%)',
+									'--clip-path-mobile': 'polygon(100% 0%, 0% 0%, 0% 10%, 100% 15%)',
+							  }
+							: {
+									'--clip-path': 'polygon(100% 0, 0 0%, 0 100%, 100% 100%)',
+									'--clip-path-mobile': 'polygon(100% 0, 0 0%, 0 100%, 100% 0%)',
+							  }
+					}
+				></div>{' '}
+				{/* Applying the style of whatever decorator color got */}
+				<div className="header">
+					<div className="title" id={decoratorColorID ? decoratorColorID : 'fighting'}>
+						{pokemonName}
+					</div>
+					<div className="type-list">
+						{' '}
+						{/* Lists out all the type list provided from props. All poke types have a styling applied by their ID.*/}
+						{type?.map((type) => {
+							return (
+								<div key={type.type.name} className="type" id={type.type.name}>
+									{toTitleCase(type.type.name)}
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				<div className="card-image">
+					<img src={image} alt="" />
+				</div>
+			</div>
 
-      <div className="preview">
-      <div className="decorator" id={decoratorColorID ? decoratorColorID : "fighting"} style={name ? {clipPath: "polygon(100% 0, 0 0, 0 100%, 0 100%)"} : {clipPath: "polygon(100% 0, 0 0%, 0 100%, 100% 100%)"}}></div> {/* Applying the style of whatever decorator color got */}
+			<span
+				className="pokeball"
+				data-scale={name ? '1.5' : '1'}
+				data-scale-mobile={name ? '1' : '0.5'}
+				style={
+					name
+						? {
+								'--transform': 'translate(-5%, -32.25%)',
+								'--transform-mobile': 'translate(-5%, -32.25%)',
 
-        <div className="header">
+								opacity: '0.5',
+								'--scale': '1.5',
+								'--scale-mobile': '1',
+						  }
+						: {
+								'--transform': 'translate(-49.5%, -49.5%)',
+								'--transform-mobile': 'translate(-110.5%, -110.5%)',
+								opacity: '1',
+								'--scale': '1',
+								'--scale-mobile': '0.45',
+						  }
+				}
+			>
+				<img src="./pokeball.svg" alt="" />
+			</span>
+			<span
+				className="pokemon-bg"
+				style={
+					name
+						? {
+								'--pokemonbg-transform': 'translate(20%, -10%)',
+								'--pokemonbg-transform-mobile': 'translate(0%, -5%)',
+								'--pokemonbg-scale': '3',
+								'--pokemonbg-scale-mobile': '1.5',
+								opacity: '0.2',
+						  }
+						: { opacity: '0' }
+				}
+			>
+				<img src={image2} alt="" />
+			</span>
 
-          <div className="title" id={decoratorColorID ? decoratorColorID : "fighting"}>
-                {pokemonName}
-          </div>
-          <div className="type-list"> {/* Lists out all the type list provided from props. All poke types have a styling applied by their ID.*/}
-            { 
-              type?.map((type) => {
-                return <div key={type.type.name} className="type" id={type.type.name}>{toTitleCase(type.type.name)}</div>
-              })
-            }
-          </div>
-        </div>
-        <div className="card-image">
-            <img src={image} alt="" />
-        </div>
-      </div>
+			<div id="info">
+				<div className="header">
+					{/* Header */}
+					<div
+						className="title"
+						style={
+							name
+								? { opacity: '1', transform: 'translateY(0px)' }
+								: { opacity: '0', transform: 'translateY(50px)' }
+						}
+					>
+						About
+					</div>
+				</div>
+				<div
+					className="content"
+					style={
+						name
+							? { opacity: '1', transform: 'translateY(0px)' }
+							: { opacity: '0', transform: 'translateY(50px)' }
+					}
+				>
+					<div id="physical-stats">
+						<div id="height">
+							<img src="https://img.icons8.com/?size=100&id=7790&format=png&color=FC405C" alt="" />
+							Height: {height}cm
+						</div>
 
-      <span className='pokeball' style={name ? {transform: "translate(-5%, -32.25%)", scale: "1.5", opacity: "0.5"} : {transform: "translate(-49.5%, -49.5%)", scale: "1", opacity: "1"}}>
-        <img src="./pokeball.svg" alt="" />
-      </span>
-      <span className='pokemon-bg' style={name ? {transform: "translate(20%, -10%)", scale: "3", opacity: "0.2"} : {opacity: "0"}}>
-        <img src={image2} alt="" />
-      </span>
+						<div className="vertical-divider"></div>
 
-      <div id="info">
-        <div className="header"> {/* Header */}
-          <div className="title" style={name ? {opacity: "1", transform: "translateY(0px)"} : {opacity: "0", transform: "translateY(50px)"}}>
-            About
-          </div>
-        </div>
-        <div className="content" style={name ? {opacity: "1", transform: "translateY(0px)"} : {opacity: "0", transform: "translateY(50px)"}}>
+						<div id="weight">
+							<img src="https://img.icons8.com/?size=100&id=4880&format=png&color=FC405C" alt="" />
+							Weight: {weight}kg
+						</div>
+					</div>
 
-          <div id="physical-stats">
-            <div id="height">
-              <img src="https://img.icons8.com/?size=100&id=7790&format=png&color=FC405C" alt="" />
-              Height: {height}cm</div>
+					<div id="other-stats">
+						<div className="stat" id="health">
+							<span>
+								<img src="https://img.icons8.com/?size=100&id=59805&format=png&color=FC405C" alt="" />
+								Health: {health}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div
+									className="progress"
+									style={{ width: `${Math.max(0, Math.min(health, 100))}%` }}
+								></div>
+							</div>
+						</div>
 
-            <div className="vertical-divider"></div>
+						<div className="stat" id="attack">
+							<span>
+								<img
+									src="https://img.icons8.com/?size=100&id=cOlYVArn39ae&format=png&color=FC405C"
+									alt=""
+								/>
+								Attack: {attack}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div
+									className="progress"
+									style={{ width: `${Math.max(0, Math.min(attack, 100))}%` }}
+								></div>
+							</div>
+						</div>
 
-            <div id="weight">
-              <img src="https://img.icons8.com/?size=100&id=4880&format=png&color=FC405C" alt="" />
-              Weight: {weight}kg</div>
-          </div>
+						<div className="stat" id="defense">
+							<span>
+								<img src="https://img.icons8.com/?size=100&id=87404&format=png&color=FC405C" alt="" />
+								Defense: {defense}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div
+									className="progress"
+									style={{ width: `${Math.max(0, Math.min(defense, 100))}%` }}
+								></div>
+							</div>
+						</div>
 
-          <div id="other-stats">
-            <div className="stat" id="health">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=59805&format=png&color=FC405C" alt="" />
-                Health: {health}
-              </span>
-              <div className="bar" style={{width: `100%`}}>
-                <div className="progress" style={{width: `${Math.max(0, Math.min(health, 100))}%`}}></div>
-              </div>
-            </div>
-
-            <div className="stat" id="attack">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=cOlYVArn39ae&format=png&color=FC405C" alt="" />
-                Attack: {attack}
-              </span>
-              <div className="bar" style={{width: `100%`}}>
-                <div className="progress" style={{width: `${Math.max(0, Math.min(attack, 100))}%`}}></div>
-              </div>
-            </div>
-
-            <div className="stat" id="defense">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=87404&format=png&color=FC405C" alt="" />
-                Defense: {defense}
-              </span>
-              <div className="bar" style={{width: `100%`}}>
-                <div className="progress" style={{width: `${Math.max(0, Math.min(defense, 100))}%`}}></div>
-              </div>
-            </div>
-
-            <div className="stat" id="special-attack">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=10535&format=png&color=FC405C" alt="" />
-                Special Attack: {sp}
-              </span>
-                <div className="bar" style={{width: `100%`}}>
-                  <div className="progress" style={{width: `${Math.max(0, Math.min(sp, 100))}%`}}></div>
-                </div>
-            </div>
-            <div className="stat" id="special-defense">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=79928&format=png&color=FC405C" alt="" />
-                Special Defense: {sd}
-              </span>
-                <div className="bar" style={{width: `100%`}}>
-                  <div className="progress" style={{width: `${Math.max(0, Math.min(sd, 100))}%`}}></div>
-              </div>
-            </div>
-            <div className="stat" id="speed">
-              <span>
-                <img src="https://img.icons8.com/?size=100&id=9806&format=png&color=FC405C" alt="" />
-                Speed: {speed}
-              </span>
-              <div className="bar" style={{width: `100%`}}>
-                <div className="progress" style={{width: `${Math.max(0, Math.min(speed, 100))}%`}}></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+						<div className="stat" id="special-attack">
+							<span>
+								<img src="https://img.icons8.com/?size=100&id=10535&format=png&color=FC405C" alt="" />
+								Special Attack: {sp}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div className="progress" style={{ width: `${Math.max(0, Math.min(sp, 100))}%` }}></div>
+							</div>
+						</div>
+						<div className="stat" id="special-defense">
+							<span>
+								<img src="https://img.icons8.com/?size=100&id=79928&format=png&color=FC405C" alt="" />
+								Special Defense: {sd}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div className="progress" style={{ width: `${Math.max(0, Math.min(sd, 100))}%` }}></div>
+							</div>
+						</div>
+						<div className="stat" id="speed">
+							<span>
+								<img src="https://img.icons8.com/?size=100&id=9806&format=png&color=FC405C" alt="" />
+								Speed: {speed}
+							</span>
+							<div className="bar" style={{ width: `100%` }}>
+								<div
+									className="progress"
+									style={{ width: `${Math.max(0, Math.min(speed, 100))}%` }}
+								></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ResultCard
